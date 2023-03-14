38a6f9f0deeb10df074344b265607e189dc6226a
saaiid <ibrosakid@gmail.com>
2023-03-14 00:40:10 -0400

    add and change pages to a new look

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,39 @@
 console.log('This is new js file')
 
 $(document).ready(function() {
- /*  console.log($('#tweet-text')) */
-  // --- our code goes here ---
-  $('#tweet-text').on('input', function() {
-    /* console.log(140 - this.value.length); */
-    let count = 140 - this.value.length;
-
-    if (count < 0) {
-      $(".counter").addClass("error");
-    } else {
-      $(".counter").removeClass("error");
  
-    }
-    $(".counter").text(count)
+  //form submition
+  $('.newtweet-container').submit(function(event) {
+     event.preventDefault();
+
+     const $form = $(this);
+     const $input = $form.find('form-textarea');
+
+     //validate tweet
+     if (!isTweetValid($input)) {
+      return false;
+     }
+
+     const data = $form.serialize();
+     $.post('/tweets', data)
+     .then(() => {
+        $input.val('')
+        $('.counter').html(140);
+        loadTweets();
+     })
+
+     //submit ajax request
+   /*   $.ajax({
+      url: '/tweets',
+      type: 'post',
+      data: data
+     }).done(function(data) {
+      $input.val('');
+      $('.counter').html(140);
+      loadTweets();
+     }) */
   });
+
+  return true;
+
 });
